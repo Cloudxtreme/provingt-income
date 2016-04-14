1e7232ef182af1d41b0e90618add3121ffa9d498
andrewmoores <a.s.moores@gmail.com>
2016-04-14 12:29:15 +0100

    Clean code version of build script.

--- build.sh
+++ build.sh
@@ -1,63 +1,79 @@
 #!/usr/bin/env bash
 set -e
+[ -n "$DEBUG" ] && set -x
 
-echo "build number:: " ${BUILD_NUMBER}
+setup() {
+	echo "build number:: " ${BUILD_NUMBER}
 
-VERSION_NUMBER=1.0
-ARTIFACT="uk.gov.digital.ho.proving.income.api-${VERSION_NUMBER}.jar"
+	VERSION_NUMBER=1.0
+	ARTIFACT="uk.gov.digital.ho.proving.income.api-${VERSION_NUMBER}.jar"
 
-if [ -n "$BUILD_NUMBER" ]; then
-	ARTIFACT="uk.gov.digital.ho.proving.income.api-${VERSION_NUMBER}.${BUILD_NUMBER}.jar"
-fi
+	if [ -n "$BUILD_NUMBER" ]; then
+		ARTIFACT="uk.gov.digital.ho.proving.income.api-${VERSION_NUMBER}.${BUILD_NUMBER}.jar"
+	fi
 
-echo "Artifact being built: " ${ARTIFACT}
+	echo "Artifact being built: " ${ARTIFACT}
+}
+
+cleanup() {
+	CONTAINERS=$(docker ps -aq)
+
+	if [ ! -z "$CONTAINERS" -a "$CONTAINERS" != " " ]; then
+		docker stop $CONTAINERS
+		docker rm $CONTAINERS
+	fi
+
+	IMAGES=$(docker images | grep build.uk.gov.digital.ho.proving.income.service | awk '{print $3'})
+
+	if [ ! -z "$IMAGES" -a "$IMAGES" != " " ]; then
+		docker rmi -f $IMAGES
+	fi
+}
+
+createDockerImageToExecuteBuildIn() {
+	echo "building docker image to execute app build"
+	docker build -f src/main/docker/Dockerfile.build -t build.uk.gov.digital.ho.proving.income.service .
+}
+
+executeBuild() {
+	echo "running docker image as named container"
+	docker run --name build.uk.gov.digital.ho.proving.income.service\
+		-e "VERSION_NUMBER=${VERSION_NUMBER}" \
+		-e "BUILD_NUMBER=${BUILD_NUMBER}" \
+		build.uk.gov.digital.ho.proving.income.service
+}
+
+collectAssetsToCreateDockerAppExecutionImage () {
+	mkdir build || true
+	mkdir build/docker || true
+	docker cp build.uk.gov.digital.ho.proving.income.service:/code/build/build/libs/$ARTIFACT build/docker/
+	cp src/main/resources/run.sh build/docker/
+	cp -R src/main/resources/json build/docker/
+	cp src/main/docker/Dockerfile build/docker/
+}
+
+buildDockerAppExecutionImage() {
+	echo "build docker image for app::"
+	cd build/docker
+	TAG="quay.io/ukhomeofficedigital/uk.gov.digital.ho.proving.income.api:${VERSION_NUMBER}.${BUILD_NUMBER}"
+	echo "building " ${TAG}
+	docker build -t ${TAG} .
+}
+
+pushImageToRepo() {
+	docker push ${TAG}
+}
 
 # -----
 # Build
 # -----
 
-# 0. Initial conditions
-docker stop $(docker ps -aq) || true
-docker rm $(docker ps -aq) || true
-docker rmi -f $(docker images | grep build.uk.gov.digital.ho.proving.income.service | awk '{print $3'}) || true
-
-# 1. Create Docker image to execute build in
-echo "building docker image to execute app build"
-docker build -f src/main/docker/Dockerfile.build -t build.uk.gov.digital.ho.proving.income.service .
-
-# 2. Run docker image, build will copy jar file into the mounted volume /artifacts
-echo "running docker image as named container"
-docker run --name build.uk.gov.digital.ho.proving.income.service\
-	-e "VERSION_NUMBER=${VERSION_NUMBER}" \
-	-e "BUILD_NUMBER=${BUILD_NUMBER}" \
-	build.uk.gov.digital.ho.proving.income.service
-
-# 2.1 Setup files for docker context
-mkdir build || true
-mkdir build/docker || true
-docker cp build.uk.gov.digital.ho.proving.income.service:/code/build/build/libs/$ARTIFACT build/docker/
-cp src/main/resources/run.sh build/docker/
-cp -R src/main/resources/json build/docker/
-cp src/main/docker/Dockerfile build/docker/
-
-# 2.2 Tidy up
-docker rm $(docker ps -aq)
-docker rmi $(docker images | grep build.uk.gov.digital.ho.proving.income.service | awk '{print $3'})
-
-# 3. Build docker image to run jar from artifacts
-echo "build docker image for app::"
-cd build/docker
-TAG="quay.io/ukhomeofficedigital/uk.gov.digital.ho.proving.income.api:${VERSION_NUMBER}.${BUILD_NUMBER}"
-echo "building " ${TAG}
-docker build -t ${TAG} .
-
-# 4. Push image to quay.io
-docker push ${TAG}
-
-if [ "$?" = "0" ]; then
-	echo "build succeeded"
-	exit 0
-else
-	echo "build failed"
-	exit 1
-fi
+setup
+cleanup
+createDockerImageToExecuteBuildIn
+executeBuild
+collectAssetsToCreateDockerAppExecutionImage
+cleanup
+buildDockerAppExecutionImage
+pushImageToRepo
+cleanup
